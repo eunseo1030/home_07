7099a5aa7c41265291b156f2e9fb51b6eb24a6ea
eunseo1030 <qordhrjaan@naver.com>
2024-07-08 20:27:44 +0900

    í3번 문제

--- src/main/java/org/korealt/Main.java
+++ src/main/java/org/korealt/Main.java
@@ -5,10 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("== 3번 문제 ==");
 
 
+        int firstnumber = 0;
+        int secondnumber = 1;
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(firstnumber + " ");
 
+            int nextnumber = firstnumber + secondnumber;
+            firstnumber = secondnumber;
+            secondnumber = nextnumber;
+
+        }
     }
 }
